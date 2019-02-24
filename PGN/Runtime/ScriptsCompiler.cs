d38cc71104fd373dba2ec627953936c41e0c761d
TheProxor <proxortv@gmail.com>
2019-02-24 11:33:23 +0700

    Roslyn success test compilation

--- PGN/Runtime/ScriptsCompiler.cs
+++ PGN/Runtime/ScriptsCompiler.cs
@@ -4,7 +4,10 @@ using System.Text;
 using System.IO;
 using System.CodeDom.Compiler;
 using System.Reflection;
-using Microsoft.CSharp;
+
+using Microsoft.CodeAnalysis.Scripting;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
 
 namespace PGN.Runtime
 {
@@ -19,53 +22,18 @@ namespace PGN.Runtime
         {
             rawCode = File.ReadAllText(path);
         }
+ 
 
         public static void TestCompile()
         {
-            CSharpCodeProvider provider = new CSharpCodeProvider();
-            CompilerParameters parameters = new CompilerParameters();
+            var scriptOptions = ScriptOptions.Default;
+            scriptOptions.AddImports("System");
+            scriptOptions.AddImports("System.Collections");
+            scriptOptions.AddImports("System.Collections.Generic");
+            scriptOptions.AddImports("System.Text");
+            scriptOptions.AddImports("System");
 
-            string code = @"
-                            using System;
-
-                            namespace First
-                            {
-                                public class Program
-                                {
-                                    public static void Main()
-                                    {
-                                    " +
-                                        "Console.WriteLine(\"THIS INVOKE FROM SCRIPT!\");"
-                                        + @"
-                                    }
-                                }
-                            }
-                        ";
-
-            // True - memory generation, false - external file generation
-            parameters.GenerateInMemory = true;
-            // True - exe file generation, false - dll file generation
-            parameters.GenerateExecutable = true;
-
-            CompilerResults results = provider.CompileAssemblyFromSource(parameters, code);
-
-            if (results.Errors.HasErrors)
-            {
-                StringBuilder sb = new StringBuilder();
-
-                foreach (CompilerError error in results.Errors)
-                {
-                    sb.AppendLine(String.Format("Error ({0}): {1}", error.ErrorNumber, error.ErrorText));
-                }
-
-                throw new InvalidOperationException(sb.ToString());
-            }
-
-            Assembly assembly = results.CompiledAssembly;
-            Type program = assembly.GetType("First.Program");
-            MethodInfo main = program.GetMethod("Main");
-
-            main.Invoke(null, null);
+            var result = CSharpScript.EvaluateAsync("Console.WriteLine(\"Hello world!\")", scriptOptions).Result;
         }
     }
 }
